2292fb592d184e8962ebedda40e54cdc9782db40
maclxf <46905313@qq.com>
2022-03-30 22:04:42 +0800

    加了一些配置说明

--- blog/.vuepress/config.ts
+++ blog/.vuepress/config.ts
@@ -1,8 +1,11 @@
+// https://v2.vuepress.vuejs.org/zh/
+// https://vuepress-theme-hope.github.io/v2/zh/changelog.html
 import { defineHopeConfig } from "vuepress-theme-hope";
 import themeConfig from "./themeConfig";
 
 export default defineHopeConfig({
   //https://vuepress.vuejs.org/zh/guide/assets.html#基础路径
+  // 主要是根据你打包的路径来如果不是跟路径就要把这个名字加上否则/即可
   base: "/maclxf.vuepress.blog.v2/",
 
   dest: "./dist",
